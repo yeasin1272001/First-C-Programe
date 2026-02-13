29d2f36815aab1283552000fb0541745600e3123
unknown <yeasinmiah1272001@gmail.com>
2026-02-13 10:52:06 +0600

    condition

---
+++ conditional.c
@@ -0,0 +1,71 @@
+/*#include<stdio.h>
+int main()
+{
+    int a,b;
+    a= 5;
+    b=10;
+    int comparisom = a>b;
+
+    printf("com: %d",comparisom);
+    return 0;
+}
+*/
+
+
+/*#include<stdio.h>
+int main()
+{
+    int a,b;
+    a= 5;
+    b=10;
+    int comparisom = a<b;
+
+    printf("com: %d",comparisom);
+    return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+    int a,b;
+    a= 5;
+    b=10;
+    int comparisom = a==b;
+
+    printf("com: %d",comparisom);
+    return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a= 10;
+    b=10;
+    int comparisom = a==b;
+
+    printf("com: %d",comparisom);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
